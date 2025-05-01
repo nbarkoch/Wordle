bc7cfa1b41da3e011a0d32fe6fc979a1986cc36c
nbarkoch <57592818+nbarkoch@users.noreply.github.com>
2025-05-01 15:23:37 +0300

    update gameSwitch

--- source/components/new/GameSwitch.tsx
+++ source/components/new/GameSwitch.tsx
@@ -37,14 +37,16 @@ const GameSwitch: React.FC<GameSwitchProps> = ({
   const [isOn, setIsOn] = useState<boolean>(defaultValue);
 
   const startColor = useSharedValue<ThemeColor>(
-    isOn ? colors.darkGreen : colors.darkRed,
+    isOn ? colors.darkGreen : colors.darkGrey,
   );
-  const endColor = useSharedValue<ThemeColor>(isOn ? colors.green : colors.red);
+  const endColor = useSharedValue<ThemeColor>(
+    isOn ? colors.green : colors.grey,
+  );
   const ballColor = useSharedValue<ThemeColor>(
-    isOn ? colors.lightGreen : colors.lightRed,
+    isOn ? colors.lightGreen : colors.lightGrey,
   );
   const borderColor = useSharedValue<ThemeColor>(
-    isOn ? colors.lightGreen : colors.lightRed,
+    isOn ? colors.lightGreen : colors.lightGrey,
   );
   const ballPosition = useSharedValue(
     isOn ? width - height + borderWidth : borderWidth,
@@ -64,10 +66,10 @@ const GameSwitch: React.FC<GameSwitchProps> = ({
           duration: 300,
         });
       } else {
-        startColor.value = withTiming(colors.darkRed);
-        endColor.value = withTiming(colors.red);
-        ballColor.value = withTiming(colors.lightRed);
-        borderColor.value = withTiming(colors.lightRed);
+        startColor.value = withTiming(colors.darkGrey);
+        endColor.value = withTiming(colors.grey);
+        ballColor.value = withTiming(colors.lightGrey);
+        borderColor.value = withTiming(colors.lightGrey);
         ballPosition.value = withTiming(borderWidth, {duration: 300});
       }
     }
@@ -97,10 +99,10 @@ const GameSwitch: React.FC<GameSwitchProps> = ({
         duration: 300,
       });
     } else {
-      startColor.value = withTiming(colors.darkRed);
-      endColor.value = withTiming(colors.red);
-      ballColor.value = withTiming(colors.lightRed);
-      borderColor.value = withTiming(colors.lightRed);
+      startColor.value = withTiming(colors.darkGrey);
+      endColor.value = withTiming(colors.grey);
+      ballColor.value = withTiming(colors.lightGrey);
+      borderColor.value = withTiming(colors.lightGrey);
       ballPosition.value = withTiming(borderWidth, {duration: 300}); // Move ball to the left
     }
     onToggle(newValue);
