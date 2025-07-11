45ad7c4f38aea0a672db3d4a49fde67dac37f8e3
nbarkoch <57592818+nbarkoch@users.noreply.github.com>
2025-07-11 17:18:53 +0300

    small fix for version 3.0

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import mobileAds from 'react-native-google-mobile-ads';
 import GameScreen from './source/screens/WordleGame';
 import {NavigationContainer} from '@react-navigation/native';
@@ -26,58 +27,67 @@ const App = () => {
   I18nManager.allowRTL(false);
 
   return (
-    <>
-      <SafeAreaView style={backgroundStyle} />
-      <StatusBar backgroundColor={colors.primary.b} />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerShown: false,
-            contentStyle: backgroundStyle,
-            animation: 'fade',
-            gestureDirection: 'horizontal',
-          }}>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              animation: 'slide_from_left',
-            }}
-          />
-          <Stack.Screen
-            name="NewGame"
-            component={NewGameScreen}
-            options={{
-              animation: 'slide_from_right',
-            }}
-          />
-          <Stack.Screen
-            name="WordGame"
-            component={GameScreen}
-            options={{
-              animation: 'slide_from_right',
-            }}
-          />
-          <Stack.Screen
-            name="UserInfo"
-            component={UserInfoScreen}
-            options={{
-              animation: 'slide_from_right',
-            }}
-          />
-          <Stack.Screen
-            name="Tutorial"
-            component={TutorialScreen}
-            options={{
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.body} edges={['top', 'left', 'right']}>
+        <StatusBar
+          backgroundColor={colors.primary.b}
+          barStyle="light-content"
+          translucent={false}
+        />
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerShown: false,
+              contentStyle: backgroundStyle,
               animation: 'fade',
-            }}
-          />
-        </Stack.Navigator>
-        <GameBannerAd />
-      </NavigationContainer>
-    </>
+              gestureDirection: 'horizontal',
+            }}>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                animation: 'slide_from_left',
+              }}
+            />
+            <Stack.Screen
+              name="NewGame"
+              component={NewGameScreen}
+              options={{
+                animation: 'slide_from_right',
+              }}
+            />
+            <Stack.Screen
+              name="WordGame"
+              component={GameScreen}
+              options={{
+                animation: 'slide_from_right',
+              }}
+            />
+            <Stack.Screen
+              name="UserInfo"
+              component={UserInfoScreen}
+              options={{
+                animation: 'slide_from_right',
+              }}
+            />
+            <Stack.Screen
+              name="Tutorial"
+              component={TutorialScreen}
+              options={{
+                animation: 'fade',
+              }}
+            />
+          </Stack.Navigator>
+          <GameBannerAd />
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  body: {flex: 1, backgroundColor: colors.primary.b},
+});
